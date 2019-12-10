118c679f2a1b016dad0b4d696c725599a4771159
ljh-c <57099181+ljh-c@users.noreply.github.com>
2019-12-09 21:06:52 -0800

    Display component appears

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Display from "./components/DisplayComponents/Display";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -17,7 +18,7 @@ function App() {
     <div className="container">
       <Logo />
       <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display />
       </div>
     </div>
   );
--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -3,6 +3,7 @@ import React from "react";
 const NumberButton = () => {
   return (
     <>
+      <Numbers />
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
